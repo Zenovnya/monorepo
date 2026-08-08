d5469a7c67b0e0aaeb0101f5d06e606d10c7ef5d
konstantinzamuraev <konstantinzamuraev@sourcecraft.dev>
2026-08-08 08:10:40 +0000

    feat(mobile): инициализация React Native + Expo приложения

---
+++ mobile/App.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import { RootNavigator } from './src/navigation';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator />
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
---
+++ mobile/src/screens/index.js
@@ -0,0 +1,5 @@
+/**
+ * Экраны приложения.
+ */
+export { default as HomeScreen } from './HomeScreen';
+export { default as SettingsScreen } from './SettingsScreen';
